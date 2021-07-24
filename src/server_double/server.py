0b1619f04928c99e2a3376a757d7fba5b6d1c4e9
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2021-07-24 13:00:39 +0200

    Move ``ConfigHandler`` logic directly into ``MockServer`` class. Fix Endpoint URL

--- src/server_double/server.py
+++ src/server_double/server.py
@@ -8,23 +8,13 @@ class ServerStatus(Enum):
     STOPPED = "stopped"
 
 
-class ConfigHandler:
-    @cherrypy.expose
-    def info(self):
-        return str(cherrypy.tree.apps)
-
-    @cherrypy.expose
-    def add_endpoint(self, url):
-        cherrypy.tree.mount(Endpoint(url=url, default_status=200), url)
-
-
 class Endpoint:
     def __init__(self, url, default_status):
         self.url = url
         self.default_status = default_status
 
     @cherrypy.expose
-    def info(self):
+    def index(self):
         return f"Endpoint {self.url}"
 
 
@@ -38,9 +28,17 @@ class MockServer:
         if self.status is ServerStatus.STARTED:
             raise RuntimeError("Mock server already started, cannot start twice.")
         self.status = ServerStatus.STARTED
-        cherrypy.tree.mount(ConfigHandler(), "/__mock", {"/": {"port": self.port}})
+        cherrypy.tree.mount(self, "/__mock", {"/": {"port": self.port}})
         cherrypy.engine.start()
 
     def stop(self):
         cherrypy.engine.exit()
         self.status = ServerStatus.STOPPED
+
+    @cherrypy.expose
+    def info(self):
+        return str(cherrypy.tree.apps)
+
+    @cherrypy.expose
+    def add_endpoint(self, url, status=200):
+        cherrypy.tree.mount(Endpoint(url=url, default_status=status), url)
--- tests/integrationtest/test_from_python_code.py
+++ tests/integrationtest/test_from_python_code.py
@@ -4,7 +4,9 @@ import pytest
 from server_double.server import MockServer
 
 
-INFO_URL = "http://localhost:8080/__mock/info"
+SERVER = "http://localhost:8080"
+
+INFO_URL = SERVER + "/__mock/info"
 
 
 @pytest.fixture
@@ -21,31 +23,51 @@ def started_server(new_server):
     yield new_server
 
 
-def test_start_server(new_server):
-    new_server.start()
-    assert requests.get(INFO_URL).status_code == 200
+class TestStartStop:
+    """Tests concerning startup and shutdown behaviour."""
+
+    @staticmethod
+    def test_start_server(new_server):
+        new_server.start()
+        assert requests.get(INFO_URL).status_code == 200
+
+    @staticmethod
+    def test_start_called_twice_raises_error(started_server):
+        """If the server was already started, a RuntimeError must be raised to inform the user
+        that the server instance is already running."""
+        with pytest.raises(RuntimeError):
+            started_server.start()
+
+    @staticmethod
+    def test_stop_running_server(started_server):
+        started_server.stop()
+        with pytest.raises(requests.ConnectionError):
+            requests.get(INFO_URL, timeout=1)
+
+    @staticmethod
+    def test_stop_already_stopped_server(started_server):
+        """It should be possible to call ``stop()`` on an already stopped server without crashing,
+        to improve stability/usability if someone accidentally calls this method twice."""
+        started_server.stop()
+        started_server.stop()
+
+    @staticmethod
+    def test_stop_never_started_server(new_server):
+        """It should be possible to call ``stop()`` on a server which was not yet started without crashing."""
+        new_server.stop()
 
 
-def test_start_called_twice_raises_error(started_server):
-    """If the server was already started, a RuntimeError must be raised to inform the user
-    that the server instance is already running."""
-    with pytest.raises(RuntimeError):
-        started_server.start()
+class TestEndpointConfiguration:
+    """Tests concerning adding, editing, and removing endpoints."""
+    ADD_ENDPOINT_URL = SERVER + "/__mock/add_endpoint"
 
+    @pytest.fixture(autouse=True)
+    def setup_server(self, started_server):
+        self.server = started_server
+        yield
 
-def test_stop_running_server(started_server):
-    started_server.stop()
-    with pytest.raises(requests.ConnectionError):
-        requests.get(INFO_URL, timeout=1)
-
-
-def test_stop_already_stopped_server(started_server):
-    """It should be possible to call ``stop()`` on an already stopped server without crashing,
-    to improve stability/usability if someone accidentally calls this method twice."""
-    started_server.stop()
-    started_server.stop()
-
-
-def test_stop_never_started_server(new_server):
-    """It should be possible to call ``stop()`` on a server which was not yet started without crashing."""
-    new_server.stop()
+    def test_add_new_endpoint(self):
+        test_url = "/my/test/endpoint"
+        self.server.add_endpoint(test_url)
+        response = requests.get(SERVER + test_url)
+        assert response.status_code == 200

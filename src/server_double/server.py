1218db203711a579cba1d0cb16cae3e0e5d6cffd
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2021-07-25 20:06:11 +0200

    Fixes from Pylint analysis

--- src/server_double/server.py
+++ src/server_double/server.py
@@ -1,33 +1,49 @@
+"""
+Logic for creating and managing the mock server.
+"""
+
 from enum import Enum
 
 import cherrypy
 
 
 class ServerStatus(Enum):
+    """Current status of the server instance."""
     STARTED = "started"
     STOPPED = "stopped"
 
 
 class Endpoint:
-    def __init__(self, url, default_status):
+    """Represents a single endpoint that was added to the ``MockServer``."""
+
+    def __init__(self, url, default_status, default_response=""):
         self.url = url
         self.default_status = default_status
+        self.default_response = default_response
 
     @cherrypy.expose
     def index(self):
-        return ""
+        """The index page returns the content that was specified when the endpoint was added or edited."""
+        return self.default_response
 
 
 class MockServer:
+    """
+    A ``MockServer`` handles the creation and lifecycle management of the underlying ``CherryPy`` server,
+    as well as acting as an URL itself and providing endpoints to add, edit and remove andpoints.
+    The underlying methods can be called from Python code directly, too.
+    """
+
     def __init__(self, config=None):
         self.config = config or {}
         self.port = self.config.get("port", 8080)
         endpoints = self.config.get("endpoints", {})
-        for endpoint, config in endpoints.items():
-            self.add_endpoint(endpoint, **config)
+        for endpoint, ep_config in endpoints.items():
+            self.add_endpoint(endpoint, **ep_config)
         self.status = ServerStatus.STOPPED
 
     def start(self):
+        """Start the ``CherryPy`` server with all endpoints given via the config or added through code."""
         if self.status is ServerStatus.STARTED:
             raise RuntimeError("Mock server already started, cannot start twice.")
         self.status = ServerStatus.STARTED
@@ -35,13 +51,16 @@ class MockServer:
         cherrypy.engine.start()
 
     def stop(self):
+        """Stop and exit the ``CherryPy`` server."""
         cherrypy.engine.exit()
         self.status = ServerStatus.STOPPED
 
     @cherrypy.expose
-    def info(self):
+    def info(self):  # pylint: disable=no-self-use
+        """Returns information of all currently configured endpoints."""
         return str(cherrypy.tree.apps)
 
     @cherrypy.expose
-    def add_endpoint(self, url, status_code=200):
+    def add_endpoint(self, url, status_code=200):  # pylint: disable=no-self-use
+        """Add a new endpoint at a given URL."""
         cherrypy.tree.mount(Endpoint(url=url, default_status=status_code), url)
